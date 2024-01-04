6b3dfed2402205a2912911253c5fae067f4da3a2
Fahrettin <ok.fahrettin@gmail.com>
2024-01-04 23:55:51 +0300

    Splash screen was developed.

--- app/src/main/java/com/enterprise/appbooks/ui/theme/Theme.kt
+++ app/src/main/java/com/enterprise/appbooks/ui/theme/Theme.kt
@@ -57,7 +57,8 @@ fun AppBooksTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            //window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = AppPrimaryColor.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }

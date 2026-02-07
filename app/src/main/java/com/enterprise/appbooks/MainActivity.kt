41d4acd10d6e55383a89ec5651828d7c242d1c93
DeveloperOk <ok.fahrettin@gmail.com>
2026-02-07 15:00:40 +0300

    Deprecated code was replaced.

--- app/src/main/java/com/enterprise/appbooks/MainActivity.kt
+++ app/src/main/java/com/enterprise/appbooks/MainActivity.kt
@@ -3,15 +3,20 @@ package com.enterprise.appbooks
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.enterprise.appbooks.presentation.navigation.BooksNavigation
 import com.enterprise.appbooks.presentation.ui.theme.AppBooksTheme
+import com.enterprise.appbooks.presentation.ui.theme.AppPrimaryColor
+import com.enterprise.appbooks.presentation.ui.theme.AppWhite
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,30 +25,37 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge()
+
         val activityFinisher : () -> Unit = {
             this.finish()
         }
 
         setContent {
-            AppBooksTheme {
-                // A surface container using the 'background' color from the theme
-
-                Surface(color = MaterialTheme.colorScheme.background,
-                    modifier = Modifier.fillMaxSize()) {
-                    Column(verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally) {
-                        BooksNavigation(activityFinisher)
-
-                    }
-
-                }
-
-
-            }
+            AppBooksApp(activityFinisher = activityFinisher)
         }
     }
 
+}
 
+@Composable
+fun AppBooksApp(activityFinisher: () -> Unit) {
 
+    AppBooksTheme {
+
+        Surface(color = AppPrimaryColor,
+            modifier = Modifier.fillMaxSize()) {
+            Column(verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.systemBarsPadding().fillMaxSize().background(AppWhite)) {
+
+                BooksNavigation(activityFinisher)
+
+            }
+
+        }
+
+    }
 
 }
+
